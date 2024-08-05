f760d83fd823c8dcec3c6d64b5885cfb22a7a579
StarsorpsDEV <starcorps@protonmail.com>
2024-08-06 01:03:21 +0300

    commit 5

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,9 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Analytics/>
-    <SpeedInsights/>
     <ThirdwebProvider>
+    <SpeedInsights/>
+    <Analytics/>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>,
