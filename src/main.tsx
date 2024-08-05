025c6af1d69bd0b0c02ba14e8522b24d2e3013f6
StarsorpsDEV <starcorps@protonmail.com>
2024-08-05 23:44:23 +0300

    commit 5

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThirdwebProvider } from "thirdweb/react";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Analytics/>
     <ThirdwebProvider>
       <App />
     </ThirdwebProvider>
