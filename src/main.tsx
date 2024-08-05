2e091352bccc91dc752af4254b7ce3e58384a75f
StarsorpsDEV <starcorps@protonmail.com>
2024-08-06 00:38:20 +0300

    commit 5

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App";
 import "./index.css";
 import { ThirdwebProvider } from "thirdweb/react";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Analytics/>
+    <SpeedInsights/>
     <ThirdwebProvider>
       <App />
     </ThirdwebProvider>
